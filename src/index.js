f892992b7a37197689a1b291eaf35d57157c14ac
unknown <jameskutty.sebastian@gmail.com>
2020-06-09 10:37:42 +0930

    added redux extension to application to view and test redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import { Provider } from 'react-redux';
 import { configureStore } from './todos/store';
-import { persustStore, persistStore } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
 const store = configureStore();
